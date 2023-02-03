a6d82e2078e1f798636333b274aaf77300399ba1
ranjeev20 <ranjeevbohara2058@gmail.com>
2023-02-03 22:22:44 +0530

    file read using csv

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const fs = require('fs');
+const csv = require('csv-parser');
 const app = express();
 
 app.get('/api/data/:branch', (req, res) => {
-  fs.readFile('./Data/bankDetails.json', 'utf-8', (err, data) => {
-    if (err) {
-      res.status(500).send({ message: 'Error reading the file' });
-      return;
-    }
-
-    const bankDetails = JSON.parse(data);
-    const result = [];
-
-    for (const bank of bankDetails) {
-        const branchName = req.params.branch.toUpperCase()
-      if (bank.branch === branchName) {
-        result.push(bank);
+  fs.createReadStream('./Data/bank_branches.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      if (data.branch === req.params.branch.toUpperCase()) {
+        result.push(data);
       }
-    }
+    })
+    .on('end', () => {
+      if (result.length === 0) {
+        res.status(404).send({ message: 'Branch not found' });
+      } else {
+        res.send(result);
+      }
+    });
 
-    if (result.length === 0) {
-      res.status(404).send({ message: 'Branch not found' });
-    } else {
-      res.send(result);
-    }
-  });
+  let result = [];
 });
 
 app.listen(5000, () => {
-  console.log('Server listening on port 5000');
+  console.log('Server listening on port 3000');
 });
